4693acc9f82ee53375e9cd7b0baf193d968b8bb5
Evon Bausa <63164022+ebauss@users.noreply.github.com>
2024-03-18 08:53:17 -0700

    Update test.py

--- chatGptModel/test.py
+++ chatGptModel/test.py
@@ -5,13 +5,13 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 def generate_text(prompt):
     inputs = tokenizer.encode(prompt, return_tensors='pt')
-    outputs = model.generate(inputs, max_length=640, pad_token_id=tokenizer.eos_token_id)
+    outputs = model.generate(inputs, max_length=64, pad_token_id=tokenizer.eos_token_id)
     generated = tokenizer.decode(outputs[0], skip_special_tokens=True)
     return generated[:generated.rfind(".")+1]
 
 
 prompt = """
-user: history of formula 1
+user: What is computer science
 """
 res = generate_text(prompt)
 print(res)
