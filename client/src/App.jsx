149f115bf0890d2f7ac6680b0bf9db7c65084af4
toco <ttachibana@my.bcit.ca>
2024-03-27 02:17:33 -0700

    feat: implement routers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+
+import Landing from "./components/landing/Landing";
+import Login from "./components/login/Login";
+import Signup from "./components/signup/Signup";
+import Query from "./components/query/Query";
 
 function App() {
-  return (
-    <>
-      <h1>COMP 4537 Project</h1>
-    </>
-  )
+	return (
+		<Routes>
+			<Route path="/" element={<Landing />} />
+			<Route path="/login" element={<Login />} />
+			<Route path="/signup" element={<Signup />} />
+			<Route path="/query" element={<Query />} />
+
+			<Route path="*" element={<Landing />} />
+		</Routes>
+	);
 }
 
-export default App
+export default App;
