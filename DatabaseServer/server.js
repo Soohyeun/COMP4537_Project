97125b9120cb274e57d0ac2a1d2aaeb981ddbb08
Terence Grigoruk <terence.grigoruk@gmail.com>
2024-03-27 19:35:24 -0700

    feat(gateway): add register route

--- DatabaseServer/server.js
+++ DatabaseServer/server.js
@@ -42,8 +42,8 @@ class DatabaseServer {
     this.app.post("/users", async (req, res) => {
       try {
         const { email, name, password } = req.body;
-        await this.db.createUser(email, name, password);
-        res.status(201).send("User created");
+        const newUser = await this.db.createUser(email, name, password);
+        res.status(201).send({ id: newUser[0].insertId });
       } catch (error) {
         console.error("Error creating user:", error);
         res.status(500).send("Error creating user");
--- GatewayServer/server.js
+++ GatewayServer/server.js
@@ -1,5 +1,10 @@
 "use strict";
 const express = require("express");
+const axios = require("axios");
+
+const AuthRootUrl = process.env.AUTH_URL || "http://localhost:8000";
+const DBRootUrl = process.env.DB_URL || "http://localhost:8080";
+const MLRootUrl = process.env.ML_URL || "http://localhost:8090";
 
 /**
  * Responsible for API server methods.
@@ -16,6 +21,36 @@ class ExpressServer {
    * Initializes the Express server.
    */
   createServer = () => {
+    // Define user routes
+    this.app.post("/auth/register", async (req, res) => {
+      try {
+        const { name, email, password } = req.body;
+        const user = await axios.get(`${DBRootUrl}/users/${email}`);
+        // check if user already exists
+        if (user.data.email) {
+          res.status(409).send(`User with email "${email}" already exists`);
+          return;
+        }
+        // TODO: get hashed password via AuthServer
+        const hashedPassword = await bcrypt.hash(password, 10);
+        // create new user in database
+        const newUser = await axios.post(
+          `${DBRootUrl}/users`,
+          { name, email, password: hashedPassword },
+          { headers: { "Content-Type": "application/json" } }
+        );
+        if (newUser.status !== 201) {
+          res.status(500).send("Error registering user");
+          return;
+        }
+        // TODO: log in user via AuthServer
+        res.status(200).json("User registered successfully");
+      } catch (error) {
+        console.error("Error registering user:", error);
+        res.status(500).send("Error registering user");
+      }
+    });
+
     // Define the default route for all other requests
     this.app.all("*", (req, res) => {
       res.status(200).send("Welcome to the server at " + new Date());
