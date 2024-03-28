35d3a37c56bd6bf2ef954081605df781feb5cf25
Terence Grigoruk <terence.grigoruk@gmail.com>
2024-03-27 20:47:02 -0700

    feat(gateway): add login route

--- GatewayServer/server.js
+++ GatewayServer/server.js
@@ -76,6 +76,32 @@ class ExpressServer {
       }
     });
 
+    this.app.post("/auth/login", async (req, res) => {
+      try {
+        const { email, password } = req.body;
+        const user = await axiosDB.get(`/users/${email}`);
+        if (!user.data.email) {
+          res.status(404).send("User not found");
+          return;
+        }
+        console.log("User data:", user.data);
+        const authResult = await axios.post(`${AuthRootUrl}/auth/login`, {
+          email,
+          password,
+          hashedPassword: user.data.password,
+        });
+        res.cookie("jwt", authResult.accessToken, {
+          secure: true,
+          httpOnly: true,
+          maxAge: 3600000,
+        });
+        res.status(200).json("User logged in successfully");
+      } catch (error) {
+        // console.error("Error logging in:", error);
+        res.status(500).send("Error logging in");
+      }
+    });
+
     // Define the default route for all other requests
     this.app.all("*", (req, res) => {
       res.status(200).send("Welcome to the server at " + new Date());
