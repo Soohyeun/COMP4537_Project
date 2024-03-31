964b5a2269c92fa067a8c75e864cfaa32b045f5d
toco <ttachibana@my.bcit.ca>
2024-03-30 21:58:41 -0700

    feat: utilise context to pass url

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,18 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import theme from "./theme";
+import URLContext from "./contexts/URLContext";
+
+const { VITE_GATEWAY_URL } = import.meta.env;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<ChakraProvider theme={theme}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</ChakraProvider>
-	</React.StrictMode>
+	<URLContext.Provider value={VITE_GATEWAY_URL || 'http://localhost:8888'}>
+		<React.StrictMode>
+			<ChakraProvider theme={theme}>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</ChakraProvider>
+		</React.StrictMode>
+	</URLContext.Provider>
 );
