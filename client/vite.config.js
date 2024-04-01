c2d9df96ef9d39a8d3e900e38719ae0ecd7b2dbb
toco <ttachibana@my.bcit.ca>
2024-03-31 23:19:43 -0700

    fix: redirect users to appropriate pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/queri-gpt/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: '/index.html',
-      },
-    },
-  },
-});
+})
